01346fbc8eefb653202bc664a833632468f8e338
Md.Al-amin <al.amin@tigerit.com>
2022-11-20 07:19:02 +0600

    Core & Servoce project separate

--- FarmFresh.Test/UnitTest1.cs
+++ FarmFresh.Test/UnitTest1.cs
@@ -34,5 +34,33 @@ namespace FarmFresh.Test
 
             Assert.AreEqual(obj1, obj);
         }
+
+        [Test]
+        public void Test2()
+        {
+            var obj = new
+            {
+                Name = "John",
+                Age = 30,
+                Address = new
+                {
+                    Street = "Main Street",
+                    City = "New York"
+                }
+            };
+
+            var obj1 = new
+            {
+                Name = "John",
+                Age = 30,
+                Address = new
+                {
+                    Street = "Main Street",
+                    City = "New York"
+                }
+            };
+
+            Assert.AreEqual(obj1, obj);
+        }
     }
 }
