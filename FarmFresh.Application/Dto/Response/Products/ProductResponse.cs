91e069d97f1cce6873b8d7d9f0372f8a04a3063b
Md.Al-amin <al.amin@tigerit.com>
2023-02-04 21:49:37 +0600

    Fix image urls name

--- FarmFresh.Application/Dto/Response/Products/ProductResponse.cs
+++ FarmFresh.Application/Dto/Response/Products/ProductResponse.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string? Description { get; set; }
-        public string? ImageUrl { get; set; }
+        public string? ImageUrls { get; set; }
         public decimal OldPrice { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
